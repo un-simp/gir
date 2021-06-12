7795b9e5276b00b38424976a07bc445d5f13759a
un_simp(PBOI) <uncraftablecreeper@gmail.com>
2021-06-12 22:23:25 +0100

    i am

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ load_dotenv(find_dotenv())
 def get_prefix(bot, message):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
-    prefixes = ['!']
+    prefixes = [';']
 
     # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
     return commands.when_mentioned_or(*prefixes)(bot, message)
